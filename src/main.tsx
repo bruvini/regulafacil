322f4f8b5e266894d6db26b707bfc11ce77dc2f4
Enfermeiro Bruno Vinícius <86611700+bruvini@users.noreply.github.com>
2025-09-03 12:34:47 -0300

    fix: restore mutation functions in cached hooks

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
-
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { CacheProvider } from './contexts/CacheContext';
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <CacheProvider>
+      <App />
+    </CacheProvider>
+  </React.StrictMode>,
+);
